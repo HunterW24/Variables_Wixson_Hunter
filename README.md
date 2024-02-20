43db620032c821effa29494b991db9a64e3ad5bf
HunterW <146263396+HunterW24@users.noreply.github.com>
2024-02-20 12:46:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables_Wixson_Hunter
+ 
---
+++ Variables_Wixson_Hunter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare an integer named "favoriteNumber".
+        int favoriteNumber;
+
+        // Initialize boolean variables "isJumping" and "isRunning" to false, on the same line.
+        bool isJumping = false, isRunning = false;
+
+        // Declare a floating point variable and name it "myFloat".
+        float myFloat;
+
+        // Assign "favoriteNumber" to your favorite number.
+        favoriteNumber = 7;
+
+        // Assign the floating point variable a floating point number.
+        myFloat = 3.14f;
+
+        // Initialize a double that you do not want to change and name it "finalGrade".
+        double finalGrade = 95.5;
+
+        // Print out all variables on their own line.
+        Console.WriteLine("favoriteNumber: " + favoriteNumber);
+        Console.WriteLine("isJumping: " + isJumping);
+        Console.WriteLine("isRunning: " + isRunning);
+        Console.WriteLine("myFloat: " + myFloat);
+        Console.WriteLine("finalGrade: " + finalGrade);
+    }
+}
+
